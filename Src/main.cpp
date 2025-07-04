ae154747f7a5039ac38e3d9b1ee14afaabfeca71
valiullin_fi <vfi@eleps.ru>
2025-07-04 13:04:35 +0300

    Плохой пример использования шаблонов

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,8 +1,25 @@
 #include "GpioDriver.hpp"
 
-int main() {
-    GpioDriver<GPIOA, 13> led;
-    while (true) {
+using led0 = GpioDriver<GpioPort::A, 0>;
+using led1 = GpioDriver<GpioPort::B, 1>;
+using led2 = GpioDriver<GpioPort::A, 5>;
 
+int main() {
+    led0::Init(led0::Mode::Output,
+               led0::OutType::PushPull,
+               led0::Pull::None,
+               led0::Speed::Medium);
+    led1::Init(led1::Mode::Output,
+               led1::OutType::OpenDrain,
+               led1::Pull::Up,
+               led1::Speed::Low);
+    led2::Init(led2::Mode::Output,
+               led2::OutType::PushPull,
+               led2::Pull::None,
+               led2::Speed::Medium);
+    while (true) {
+        led0::Toggle();
+        led1::Toggle();
+        led2::Toggle();
     }
 }
